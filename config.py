6ed3588f6a62ccf2deaff117798ddebdaf66d49c
PlatMatvey <matvey0910008@email.com>
2026-06-13 16:17:48 +0600

    Кemove duplicate modules from project root

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# На Linux FFmpeg вызывается глобальной системной командой
-FFMPEG_EXE = "ffmpeg"
-
-# Корневая папка для хранения твоей медиатеки
-BASE_OUTPUT_DIR = "./Videos"
-
-os.makedirs(BASE_OUTPUT_DIR, exist_ok=True)
